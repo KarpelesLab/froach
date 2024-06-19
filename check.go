2b698a6feeb44fd2dda81ea72eaf545c9c7b2f4e
Mark Karpeles <magicaltux@gmail.com>
2024-06-19 16:09:26 +0900

    split monitor from cmd

---
+++ check.go
@@ -0,0 +1,71 @@
+package froach
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/KarpelesLab/runutil"
+)
+
+// Monitor will check if cockroachdb is launched every 1 min and launch it if needed
+func Monitor() {
+	time.Sleep(5 * time.Second)
+	cockroachCheck()
+
+	// initialize ticker only after running once since first run can take longer (cockroach download, etc)
+	t := time.NewTicker(time.Minute)
+
+	for _ = range t.C {
+		cockroachCheck()
+	}
+}
+
+func cockroachCheck() {
+	defer func() {
+		if e := recover(); e != nil {
+			slog.Error(fmt.Sprintf("cockroach check panic: %s", e), "event", "froach:check:panic")
+		}
+	}()
+
+	if err := check(); err != nil {
+		slog.Error(fmt.Sprintf("cockroach run error: %s", err), "event", "froach:check:error")
+	}
+}
+
+func check() error {
+	// first, let's check if cockroach is already running
+	pids := runutil.PidOf("cockroach")
+	if len(pids) > 0 {
+		// already got a cockroach process out there
+		return nil
+	}
+
+	exe, err := Exe()
+	if err != nil {
+		return err
+	}
+
+	// let's get a list of peers
+	peers := getAddrs()
+
+	// make cmdline
+	cmdline := makeCmdline(peers)
+
+	// prepare command
+	c := exec.Command(exe, cmdline...)
+	c.Stdin = os.Stdin
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+
+	slog.Debug(fmt.Sprintf("[pgdb] about to launch: %s", c), "event", "mercury:pgdb:run")
+
+	err = c.Start()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- cmd.go
+++ cmd.go
@@ -1,77 +1,12 @@
 package froach
 
 import (
-	"fmt"
-	"log/slog"
-	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/KarpelesLab/cloudinfo"
 	"github.com/KarpelesLab/goupd"
-	"github.com/KarpelesLab/runutil"
 )
-
-func Monitor() {
-	time.Sleep(5 * time.Second)
-	cockroachCheck()
-
-	// initialize ticker only after running once since first run can take longer (cockroach download, etc)
-	t := time.NewTicker(time.Minute)
-
-	for _ = range t.C {
-		cockroachCheck()
-	}
-}
-
-func cockroachCheck() {
-	defer func() {
-		if e := recover(); e != nil {
-			slog.Error(fmt.Sprintf("cockroach check panic: %s", e), "event", "froach:check:panic")
-		}
-	}()
-
-	if err := check(); err != nil {
-		slog.Error(fmt.Sprintf("cockroach run error: %s", err), "event", "froach:check:error")
-	}
-}
-
-func check() error {
-	// first, let's check if cockroach is already running
-	pids := runutil.PidOf("cockroach")
-	if len(pids) > 0 {
-		// already got a cockroach process out there
-		return nil
-	}
-
-	exe, err := Exe()
-	if err != nil {
-		return err
-	}
-
-	// let's get a list of peers
-	peers := getAddrs()
-
-	// make cmdline
-	cmdline := makeCmdline(peers)
-
-	// prepare command
-	c := exec.Command(exe, cmdline...)
-	c.Stdin = os.Stdin
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-
-	slog.Debug(fmt.Sprintf("[pgdb] about to launch: %s", c), "event", "mercury:pgdb:run")
-
-	err = c.Start()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
 
 func makeCmdline(peers []string) []string {
 	// check for goupd flags
@@ -79,7 +14,7 @@ func makeCmdline(peers []string) []string {
 		return []string{
 			"start-single-node",
 			"--insecure",
-			"--store=type=mem,size=50%",
+			"--store=type=mem,size=50%", // will disappear on stop
 			"--listen-addr=localhost:36257",
 			"--sql-addr=localhost:26257",
 			"--accept-sql-without-tls",
